a9ff27a1b19a9c59edd17c051f2af74524fd54f9
Sagar Bharati <bharatisagar678@gmail.com>
2024-10-18 09:49:46 +0530

    downloading a piece

--- app/main.py
+++ app/main.py
@@ -7,6 +7,86 @@ import requests
 import struct
 # import requests - available if you need it!
 import hashlib
+
+
+
+def download_piece(decoded_data, info_hash, piece_index, output_file):
+    peers = get_peers(decoded_data, info_hash)
+    peer_ip, peer_port = peers[0].split(":")
+    peer_port = int(peer_port)
+    get_peer_id(peer_ip, peer_port, info_hash)
+    protocol_name_length = struct.pack(">B", 19)
+    protocol_name = b"BitTorrent protocol"
+    reserved_bytes = b"\x00" * 8
+    peer_id = b"PC0001-7694471987235"
+    payload = (
+        protocol_name_length + protocol_name + reserved_bytes + info_hash + peer_id
+    )
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.connect((peer_ip, peer_port))
+        sock.sendall(payload)
+        response = sock.recv(68)
+        message = receive_message(sock)
+        while int(message[4]) != 5:
+            message = receive_message(sock)
+        interested_payload = struct.pack(">IB", 1, 2)
+        sock.sendall(interested_payload)
+        message = receive_message(sock)
+        while int(message[4]) != 1:
+            message = receive_message(sock)
+        file_length = decoded_data["info"]["length"]
+        total_number_of_pieces = len(
+            extract_pieces_hashes(decoded_data["info"]["pieces"])
+        )
+        default_piece_length = decoded_data["info"]["piece length"]
+        if piece_index == total_number_of_pieces - 1:
+            piece_length = file_length - (default_piece_length * piece_index)
+        else:
+            piece_length = default_piece_length
+        number_of_blocks = math.ceil(piece_length / (16 * 1024))
+        data = bytearray()
+        for block_index in range(number_of_blocks):
+            begin = 2**14 * block_index
+            print(f"begin: {begin}")
+            block_length = min(piece_length - begin, 2**14)
+            print(
+                f"Requesting block {block_index + 1} of {number_of_blocks} with length {block_length}"
+            )
+            request_payload = struct.pack(
+                ">IBIII", 13, 6, piece_index, begin, block_length
+            )
+            print("Requesting block, with payload:")
+            print(request_payload)
+            print(struct.unpack(">IBIII", request_payload))
+            print(int.from_bytes(request_payload[:4]))
+            print(int.from_bytes(request_payload[4:5]))
+            print(int.from_bytes(request_payload[5:9]))
+            print(int.from_bytes(request_payload[17:21]))
+            sock.sendall(request_payload)
+            message = receive_message(sock)
+            data.extend(message[13:])
+        with open(output_file, "wb") as f:
+            f.write(data)
+    finally:
+        sock.close()
+    return True
+def receive_message(s):
+    length = s.recv(4)
+    while not length or not int.from_bytes(length):
+        length = s.recv(4)
+    message = s.recv(int.from_bytes(length))
+    # If we didn't receive the full message for some reason, keep gobbling.
+    while len(message) < int.from_bytes(length):
+        message += s.recv(int.from_bytes(length) - len(message))
+    return length + message
+
+
+
+
+
+
+
 def decode_part(value, start_index):
     if chr(value[start_index]).isdigit():
         return decode_string(value, start_index)
@@ -142,6 +222,22 @@ def main():
                 s.connect((ip, int(port)))
                 s.send(handshake)
                 print(f"Peer ID: {s.recv(68)[48:].hex()}")
+    elif command=='download_piece':
+            output_file = sys.argv[3]
+            piece_index = int(sys.argv[5])
+            torrent_file = sys.argv[4]
+            with open(torrent_file, "rb") as f:
+                torrent_data = f.read()
+            decoded_data = decode_bencode(torrent_data)[0]
+            if download_piece(
+                decoded_data,
+                hashlib.sha1(extract_info_hash(torrent_data)).digest(),
+                piece_index,
+                output_file,
+            ):
+                print(f"Piece {piece_index} downloaded to {output_file}.")
+            else:
+                raise RuntimeError("Failed to download piece")
 
 
     else:
